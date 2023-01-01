e2dce8c3bbac0c2d08419a513cb246923a90d3cb
lutfahimayatus <105587816+lutfahimayatus@users.noreply.github.com>
2023-01-01 20:47:43 +0700

    delete

--- admin/dashboard/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "dashboard";
-echo "<br>";
-var_dump($_SERVER);
-var_dump($_SERVER['REQUEST_URI']);
-echo str_contains($_SERVER['REQUEST_URI'], 'dashboard');
--- admin/pemasok/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-// delete data
-if (isset($_GET['id'])) {
-    require_once '../controllers/PemasokController.php';
-    $produkController = new PemasokController();
-    $produkController->hapus_pemasok($_GET['id']);
-} else {
-    header('location: ../404.php');
-}
